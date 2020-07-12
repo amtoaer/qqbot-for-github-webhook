ef8cbc8fc4aa4122717c96cf9bb4bc053548e0c3
amtoaer <amtoaer@outlook.com>
2020-07-12 10:31:41 +0800

    modify comment

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import asyncio
 import threading
 import webhook_listener
 import json
-# your QQ robot
+# 你的bot的qq号
 qq = 123456789
-# your auth_key
+# 你的auth_key
 authKey = 'INITKEY6QQze5JY'
-# mirai api http locate
+# mirai api地址
 mirai_api_http_locate = 'localhost:5742/'
 
 app = Mirai(f"mirai://{mirai_api_http_locate}?authKey={authKey}&qq={qq}")
@@ -40,16 +40,25 @@ def handleWebhook(request, *args, **kwargs):
                                content['pusher']['name'])
     for item in content['commits']:
         printContent += commitMessage.format(item['id'][0:7], item['message'])
+    # 私聊通知给某位好友
     asyncio.run_coroutine_threadsafe(app.sendFriendMessage(
-        # receiver's QQ
+        # 好友的QQ
         123456789,
         [Plain(text=printContent)]
     ), loop)
+    '''
+    如果你需要给群组发送通知，那么应修改为
+    asyncio.run_coroutine_threadsafe(app.sendGroupMessage(
+        # QQ群号
+        123456789,
+        [Plain(text=printContent)]
+    ), loop)
+    '''
 
 
 def main():
     webhooks = webhook_listener.Listener(
-        # webhook's listen port
+        # webhook的监听端口
         handlers={"POST": handleWebhook}, port=12345)
     webhooks.start()
 
